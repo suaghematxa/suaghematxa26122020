80e0ca94057c87b4193aa169e9bdbc6e70ed15c5
suaghemassage247 <giagocgiasoc@gmail.com>
2020-07-26 16:24:00 +0700

    update

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -5,7 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
   // Define changes to default configuration here. For example:
-    config.language = 'fr';
+    config.language = 'vn';
     // config.uiColor = '#AADC6E';
     config.htmlEncodeOutput = false;
     config.entities = false;
