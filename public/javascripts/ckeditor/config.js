0b2519de48a786177b8feaeea05da61edc641cfc
suaghemassage247 <giagocgiasoc@gmail.com>
2020-07-26 16:02:25 +0700

    update

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -4,7 +4,12 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+  // Define changes to default configuration here. For example:
+    config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.htmlEncodeOutput = false;
+    config.entities = false;
+    config.entities_latin = false;
+    config.ForceSimpleAmpersand = true;
+    config.ProcessHTMLEntities = false ;
 };
