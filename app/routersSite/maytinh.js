e3332c3ebd645d4519364f861657944c52c1d0ea
suaghemassage247 <giagocgiasoc@gmail.com>
2020-08-05 21:21:40 +0700

    update

--- app/routersSite/maytinh.js
+++ app/routersSite/maytinh.js
@@ -4,6 +4,7 @@ const MaytinhController = require('../controllers/site/maytinh');
 var Cart = require('../models/cart');
  var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+router.get('/thao-lap-di-doi-van-chuyen-ghe-massage',MaytinhController.vanchuyenlapdatghemassage_get_home);
 router.get('/dich-vu-sua-ghe-massage',MaytinhController.suaghemassage_get_home);
 router.get('/sua-ghe-massage-hung-yen',MaytinhController.suaghemassagehungyen_get_home);
 router.get('/sua-ghe-massage-xuan-mai',MaytinhController.suaghemassagexuanmai_get_home);
